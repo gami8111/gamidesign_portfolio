4f46d5286ec47739c9417e0743a4580c11341e43
gami8111 <105031111+gami8111@users.noreply.github.com>
2022-05-22 04:50:02 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+{
+  // Intersection Observer API
+
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+    rootMargin: '0px 0px -100px',
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const targets = document.querySelectorAll('.animate');
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+}
